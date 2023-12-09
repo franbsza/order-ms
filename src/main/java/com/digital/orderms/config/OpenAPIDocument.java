7bf66b38a18e18ffe2b154947e444624a56e08d6
franbsz <franciele.souza@cayena.com>
2023-12-09 19:43:32 -0300

    feat: config cors and swagger auth

--- src/main/java/com/digital/orderms/config/OpenAPIDocument.java
+++ src/main/java/com/digital/orderms/config/OpenAPIDocument.java
@@ -1,7 +1,10 @@
 package com.digital.orderms.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +12,17 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAPIDocument {
     @Bean
     public OpenAPI microserviceOpenAPI() {
-        return new OpenAPI().info(
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(
                 new Info()
                         .title("Order Mngmt API")
                         .description("The Order Mngmt API aims to provide a way to manage orders.")
--- src/main/java/com/digital/orderms/config/security/ResourceServerConfig.java
+++ src/main/java/com/digital/orderms/config/security/ResourceServerConfig.java
@@ -1,11 +1,17 @@
 package com.digital.orderms.config.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -17,7 +23,8 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(final HttpSecurity http) throws Exception {
         http
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and()
+                .cors().configurationSource(corsConfiguration())
+                .and()
                 .csrf().disable()
                 .authorizeRequests(r -> r.antMatchers(
                                 "/v3/api-docs/**",
@@ -29,5 +36,27 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                                 .authenticated())
                 .oauth2ResourceServer()
                 .jwt().jwtAuthenticationConverter(keycloakJwtAuthenticationConverter);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("PATCH");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("OPTIONS");
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type",
+                "Content-Type", "Referer", "Accept", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers"));
+        corsConfig.setExposedHeaders(Arrays.asList("X-Get-Header"));
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
